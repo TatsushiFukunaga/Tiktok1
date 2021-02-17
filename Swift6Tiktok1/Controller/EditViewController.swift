c9599fbc02c97bb96582f5828b65f12a8eff3c37
tatsushi.fukunaga <tatsushi1789@gmail.com>
2021-02-17 15:28:56 +0900

    SwiftyCamのコード

---
+++ Swift6Tiktok1/Controller/EditViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EditViewViewController.swift
+//  Swift6Tiktok1
+//
+//  Created by Tatsushi Fukunaga on 2021/02/17.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    var url :URL?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
