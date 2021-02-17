c46a057af17b2cd2e6d0bfe3fc50f76846c259fb
tatsushi.fukunaga <tatsushi1789@gmail.com>
2021-02-17 16:40:44 +0900

    setUPVideoPlayer()でPlayerController

--- Swift6Tiktok1/Controller/EditViewController.swift
+++ Swift6Tiktok1/Controller/EditViewController.swift
@@ -6,15 +6,46 @@
 //
 
 import UIKit
+import AVKit
 
 class EditViewController: UIViewController {
 
-    var url :URL?
+    var url:URL?
+    var playerController:AVPlayerViewController?
+    var player:AVPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setUPVideoPlayer(url: url!)
+    }
+    
+    func setUPVideoPlayer(url:URL) {
+        
+        playerController?.removeFromParent()
+        player = nil
+        
+        view.backgroundColor = .black
+        
+        playerController = AVPlayerViewController()
+        playerController?.videoGravity = .resizeAspectFill
+        playerController?.view.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height - 100)
+        playerController?.showsPlaybackControls = false
+        playerController?.player = player!
+        self.addChild(playerController!)
+        self.view.addSubview((playerController?.view)!)
+        
+        player?.play()
     }
     
 
