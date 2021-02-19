82912cdf595f38033ec304a4c3ae423214579c20
tatsushi.fukunaga <tatsushi1789@gmail.com>
2021-02-19 16:03:45 +0900

    keyboardの動きに合わせてviewをずらす

--- Swift6Tiktok1/Controller/ShareViewController.swift
+++ Swift6Tiktok1/Controller/ShareViewController.swift
@@ -6,16 +6,41 @@
 //
 
 import UIKit
+import AVKit
+import Photos
 
 class ShareViewController: UIViewController {
 
     var captionString = String()
     var passedURL = String()
+    var player: AVPlayer?
+    var playerController: AVPlayerViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let notification = NotificationCenter.default
+        
+        notification.addObserver(self, selector: #selector(self.keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        notification.addObserver(self, selector: #selector(self.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
 
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func keyboardWillShow(notification: Notification?){
+        let rect = (notification?.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+        let duration: TimeInterval? = notification?.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
+        UIView.animate(withDuration: duration!) {
+            self.view.transform = CGAffineTransform(translationX: 0, y: -(rect?.size.height)!)
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: Notification?){
+        let duration: TimeInterval? = notification?.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Double
+        UIView.animate(withDuration: duration!) {
+            self.view.transform = CGAffineTransform.identity
+        }
     }
     
 
